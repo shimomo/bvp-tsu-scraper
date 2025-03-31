07d268bb767320119617d14a4bdbd1f072cfbb09
shimomo <developer@shimomo.net>
2025-03-31 16:07:35 +0900

    test: Add tests for instance retrieval, creation, and reset

--- tests/ScraperTest.php
+++ tests/ScraperTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace BVP\TsuScraper\Tests;
 
 use BVP\TsuScraper\Scraper;
+use BVP\TsuScraper\ScraperInterface;
 use PHPUnit\Framework\Attributes\DataProviderExternal;
 use PHPUnit\Framework\TestCase;
 
@@ -79,5 +80,35 @@ final class ScraperTest extends TestCase
         );
 
         Scraper::invalid(1, '2025-01-06');
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetInstance(): void
+    {
+        Scraper::resetInstance();
+        $this->assertInstanceOf(ScraperInterface::class, Scraper::getInstance());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCreateInstance(): void
+    {
+        Scraper::resetInstance();
+        $this->assertInstanceOf(ScraperInterface::class, Scraper::createInstance());
+    }
+
+    /**
+     * @return void
+     */
+    public function testResetInstance(): void
+    {
+        Scraper::resetInstance();
+        $instance1 = Scraper::getInstance();
+        Scraper::resetInstance();
+        $instance2 = Scraper::getInstance();
+        $this->assertNotSame($instance1, $instance2);
     }
 }
