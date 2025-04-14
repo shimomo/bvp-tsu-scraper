1610dcfd1bfb27ad119d36297f2eeddfc1e41b58
shimomo <developer@shimomo.net>
2025-04-15 01:40:35 +0900

    test: Rename `invalid()` to `ghost()` in `TimeScraperTest`

--- tests/Scrapers/TimeScraperTest.php
+++ tests/Scrapers/TimeScraperTest.php
@@ -48,9 +48,9 @@ final class TimeScraperTest extends TestCase
         $this->expectException(\BadMethodCallException::class);
         $this->expectExceptionMessage(
             "BVP\TsuScraper\Scrapers\BaseScraper::__call() - " .
-            "Call to undefined method 'BVP\TsuScraper\Scrapers\BaseScraper::invalid()'."
+            "Call to undefined method 'BVP\TsuScraper\Scrapers\BaseScraper::ghost()'."
         );
 
-        $this->scraper->invalid(1, '2025-01-06');
+        $this->scraper->ghost(1, '2025-01-06');
     }
 }
